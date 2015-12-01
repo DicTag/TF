79c598608202604d0b66944631900519fe157bd7
DicTag <flyyufeiyu@gmail.com>
2015-12-01 21:04:49 +0800

    重构调用的非白名单函数

--- 0TF_Base/TF.lua
+++ 0TF_Base/TF.lua
@@ -2159,6 +2159,220 @@ TF.OnDocumentComplete = function()
 	end
 end
 
+------------------------------------
+--Compatible
+------------------------------------
+-- internet exploere
+if not OpenInternetExplorer then
+function IsInternetExplorerOpened(nIndex)
+	local frame = Station.Lookup("Topmost/IE"..nIndex)
+	if frame and frame:IsVisible() then
+		return true
+	end
+	return false
+end
+
+function IE_GetNewIEFramePos()
+	local nLastTime = 0
+	local nLastIndex = nil
+	for i = 1, 10, 1 do
+		local frame = Station.Lookup("Topmost/IE"..i)
+		if frame and frame:IsVisible() then
+			if frame.nOpenTime > nLastTime then
+				nLastTime = frame.nOpenTime
+				nLastIndex = i
+			end
+		end
+	end
+	if nLastIndex then
+		local frame = Station.Lookup("Topmost/IE"..nLastIndex)
+		x, y = frame:GetAbsPos()
+		local wC, hC = Station.GetClientSize()
+		if x + 890 <= wC and y + 630 <= hC then
+			return x + 30, y + 30
+		end
+	end
+	return 40, 40
+end
+
+function OpenInternetExplorer(szAddr, bDisableSound)
+	local nIndex, nLast = nil, nil
+	for i = 1, 10, 1 do
+		if not IsInternetExplorerOpened(i) then
+			nIndex = i
+			break
+		elseif not nLast then
+			nLast = i
+		end
+	end
+	if not nIndex then
+		OutputMessage("MSG_ANNOUNCE_RED", g_tStrings.MSG_OPEN_TOO_MANY)
+		return nil
+	end
+	local x, y = IE_GetNewIEFramePos()
+	local frame = Wnd.OpenWindow("InternetExplorer", "IE"..nIndex)
+	frame.bIE = true
+	frame.nIndex = nIndex
+
+	frame:BringToTop()
+	if nLast then
+		frame:SetAbsPos(x, y)
+		frame:CorrectPos()
+		frame.x = x
+		frame.y = y
+	else
+		frame:SetPoint("CENTER", 0, 0, "CENTER", 0, 0)
+		frame.x, frame.y = frame:GetAbsPos()
+	end
+	local webPage = frame:Lookup("WebPage_Page")
+	if szAddr then
+		webPage:Navigate(szAddr)
+	end
+	Station.SetFocusWindow(webPage)
+	if not bDisableSound then
+		PlaySound(SOUND.UI_SOUND,g_sound.OpenFrame)
+	end
+	return webPage
+end
+end
+
+-- dialogue panel
+if not IsDialoguePanelOpened then
+function IsDialoguePanelOpened()
+	local frame = Station.Lookup("Normal/DialoguePanel")
+	if frame and frame:IsVisible() then
+		return true
+	end
+	return false
+end
+end
+
+-- hotkey panel
+function HotkeyPanel_Open(szGroup)
+	local frame = Station.Lookup("Topmost/HotkeyPanel")
+	if not frame then
+		frame = Wnd.OpenWindow("HotkeyPanel")
+	elseif not frame:IsVisible() then
+		frame:Show()
+	end
+	if not szGroup then return end
+	-- load aKey
+	local aKey, nI, bindings = nil, 0, Hotkey.GetBinding(false)
+	for k, v in pairs(bindings) do
+		if v.szHeader ~= "" then
+			if aKey then
+				break
+			elseif v.szHeader == szGroup then
+				aKey = {}
+			else
+				nI = nI + 1
+			end
+		end
+		if aKey then
+			if not v.Hotkey1 then
+				v.Hotkey1 = {nKey = 0, bShift = false, bCtrl = false, bAlt = false}
+			end
+			if not v.Hotkey2 then
+				v.Hotkey2 = {nKey = 0, bShift = false, bCtrl = false, bAlt = false}
+			end
+			table.insert(aKey, v)
+		end
+	end
+	if not aKey then return end
+	local hP = frame:Lookup("", "Handle_List")
+	local hI = hP:Lookup(nI)
+	if hI.bSel then return end
+	-- update list effect
+	for i = 0, hP:GetItemCount() - 1 do
+		local hB = hP:Lookup(i)
+		if hB.bSel then
+			hB.bSel = false
+			if hB.IsOver then
+				hB:Lookup("Image_Sel"):SetAlpha(128)
+				hB:Lookup("Image_Sel"):Show()
+			else
+				hB:Lookup("Image_Sel"):Hide()
+			end
+		end
+	end
+	hI.bSel = true
+	hI:Lookup("Image_Sel"):SetAlpha(255)
+	hI:Lookup("Image_Sel"):Show()
+	-- update content keys [hI.nGroupIndex]
+	local hK = frame:Lookup("", "Handle_Hotkey")
+	local szIniFile = "UI/Config/default/HotkeyPanel.ini"
+	Hotkey.SetCapture(false)
+	hK:Clear()
+	hK.nGroupIndex = hI.nGroupIndex
+	hK:AppendItemFromIni(szIniFile, "Text_GroupName")
+	hK:Lookup(0):SetText(szGroup)
+	hK:Lookup(0).bGroup = true
+	for k, v in ipairs(aKey) do
+		hK:AppendItemFromIni(szIniFile, "Handle_Binding")
+		local hI = hK:Lookup(k)
+		hI.bBinding = true
+		hI.nIndex = k
+		hI.szTip = v.szTip
+		hI:Lookup("Text_Name"):SetText(v.szDesc)
+		for i = 1, 2, 1 do
+			local hK = hI:Lookup("Handle_Key"..i)
+			hK.bKey = true
+			hK.nIndex = i
+			local hotkey = v["Hotkey"..i]
+			hotkey.bUnchangeable = v.bUnchangeable
+			hK.bUnchangeable = v.bUnchangeable
+			local text = hK:Lookup("Text_Key"..i)
+			text:SetText(GetKeyShow(hotkey.nKey, hotkey.bShift, hotkey.bCtrl, hotkey.bAlt))
+			-- update btn
+			if hK.bUnchangeable then
+				hK:Lookup("Image_Key"..hK.nIndex):SetFrame(56)
+			elseif hK.bDown then
+				hK:Lookup("Image_Key"..hK.nIndex):SetFrame(55)
+			elseif hK.bRDown then
+				hK:Lookup("Image_Key"..hK.nIndex):SetFrame(55)
+			elseif hK.bSel then
+				hK:Lookup("Image_Key"..hK.nIndex):SetFrame(55)
+			elseif hK.bOver then
+				hK:Lookup("Image_Key"..hK.nIndex):SetFrame(54)
+			elseif hotkey.bChange then
+				hK:Lookup("Image_Key"..hK.nIndex):SetFrame(56)
+			elseif hotkey.bConflict then
+				hK:Lookup("Image_Key"..hK.nIndex):SetFrame(54)
+			else
+				hK:Lookup("Image_Key"..hK.nIndex):SetFrame(53)
+			end
+		end
+	end
+	-- update content scroll
+	hK:FormatAllItemPos()
+	local wAll, hAll = hK:GetAllItemSize()
+    local w, h = hK:GetSize()
+    local scroll = frame:Lookup("Scroll_Key")
+    local nCountStep = math.ceil((hAll - h) / 10)
+    scroll:SetStepCount(nCountStep)
+	scroll:SetScrollPos(0)
+	if nCountStep > 0 then
+		scroll:Show()
+    	scroll:GetParent():Lookup("Btn_Up"):Show()
+    	scroll:GetParent():Lookup("Btn_Down"):Show()
+    else
+    	scroll:Hide()
+    	scroll:GetParent():Lookup("Btn_Up"):Hide()
+    	scroll:GetParent():Lookup("Btn_Down"):Hide()
+    end
+	-- update list scroll
+	local scroll = frame:Lookup("Scroll_List")
+	if scroll:GetStepCount() > 0 then
+		local _, nH = hI:GetSize()
+		local nStep = math.ceil((nI * nH) / 10)
+		if nStep > scroll:GetStepCount() then
+			nStep = scroll:GetStepCount()
+		end
+		scroll:SetScrollPos(nStep)
+	end
+end
+
+
 --------------------------------------------------------------------
 -- 注册事件、初始化
 ---------------------------------------------------------------------
